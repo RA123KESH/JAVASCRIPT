8eb10482dd18506d1c3d07b27d72a92a350c981a
RA123KESH <kumarrakesh8287@gmail.com>
2024-03-14 22:07:32 +0530

    Array session

---
+++ 01_Basics/Array.js
@@ -0,0 +1,24 @@
+//Array---> Array is a collection of items.--> push inset in the last item
+
+const a = [52,34,31,45,54,37]
+//how to add a items in a array
+a.push(42)
+console.log(a);
+
+// how to delete a items in array--> Pop delete the last elements
+
+a.pop()
+console.log(a);
+// unshift is used to add the items in the first position array list
+a.unshift(51)
+console.log(a);
+//Shift is used to delete the items in the first position array list.
+a.shift()
+console.log(a);
+//include is used to check whether an item is available or not. If item is available output will be true.
+console.log(a.includes(40));
+
+function x(){
+
+}
+
